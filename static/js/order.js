23109850a42a7485403e1ce61517341c4329167e
ianshen <ian.shen.sense@gmail.com>
2014-04-08 18:03:40 +0800

    60

--- static/js/order.js
+++ static/js/order.js
@@ -10,23 +10,50 @@ $(function() {
 	$("a.js-decrement").die().live("click", function() {
 		var curNumObj = $(this).parent().find("em.quantity-text");
 		var curNum = parseInt(curNumObj.text());
+		var id = $(this).parent().parent().attr("data-id");
 		if (curNum <= 1) {
-			confirm("确定从购物车中删除此商品？");
-		}
-		$.ajax({
-			async : false,
-			type : "POST",
-			url : $uroot + 'order/uc',
-			data : "",
-			success : function(data) {
-				var data = $.parseJSON(data);
-				if (data.status == 100000) {
-					// 更新数量
-					curNumObj.text(curNum - 1);
-					// 更新总价
+			art.dialog({
+				id : 'id-js-rm-product',
+				title : '删除商品',
+				follow : this,
+				content : '<div style="color:#666666;font-size:12px;">确定删除此商品吗？</div>',
+				okValue : '确定',
+				width : '15em',
+				ok : function() {
+					$.ajax({
+						async : false,
+						type : "POST",
+						url : $uroot + 'order/uc',
+						data : "",
+						success : function(data) {
+							var data = $.parseJSON(data);
+							if (data.status == 100000) {
+								$(".table.product-cart").find("tr[data-id='"+id+"']").remove();
+								var len=$(".table.product-cart tbody tr").length;
+								if (len < 1) {
+									window.location.reload(true);
+								}
+							}
+						}
+					});
 				}
-			}
-		});
+			});
+		}else{
+			$.ajax({
+				async : false,
+				type : "POST",
+				url : $uroot + 'order/uc',
+				data : "",
+				success : function(data) {
+					var data = $.parseJSON(data);
+					if (data.status == 100000) {
+						// 更新数量
+						curNumObj.text(curNum - 1);
+						// 更新总价
+					}
+				}
+			});
+		}
 	});
 	// 购物车数量加一
 	$("a.js-increment").die().live("click", function() {
@@ -67,8 +94,11 @@ $(function() {
 					success : function(data) {
 						var data = $.parseJSON(data);
 						if (data.status == 100000) {
-							//$(".table.product-cart").find("tr[data-id='"+id+"']").remove();
-							window.location.reload(true);
+							$(".table.product-cart").find("tr[data-id='"+id+"']").remove();
+							var len=$(".table.product-cart tbody tr").length;
+							if (len < 1) {
+								window.location.reload(true);
+							}
 						}
 					}
 				});
