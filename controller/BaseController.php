1f806ef70fc16da03a1f6a86afd7c217de4ef26e
ianshen <ian.shen.sense@gmail.com>
2014-04-21 18:36:00 +0800

    79

--- controller/BaseController.php
+++ controller/BaseController.php
@@ -22,4 +22,26 @@ class BaseController extends Cola_Controller {
     protected function isLogin() {
         return false;
     }
+    
+    /**
+     * 获取某分类的购物车
+     * @param unknown_type $categoryId
+     * @return multitype:multitype: number unknown 
+     */
+    protected function getCart($categoryId) {
+        $cart = array ();
+        $cart ['products'] = array ();
+        $cart ['totalPrice'] = 0;
+        if (isset ( $_SESSION ['cart'] [$categoryId] )) {
+            $products = $_SESSION ['cart'] [$categoryId];
+            $totalPrice = 0;
+            foreach ( $products as &$v ) {
+                $v ['thisTotalPrice'] = intval ( $v ['price'] ) * intval ( $v ['quantity'] );
+                $totalPrice += $v ['thisTotalPrice'];
+            }
+            $cart ['products'] = $products;
+            $cart ['totalPrice'] = $totalPrice;
+        }
+        return $cart;
+    }
 }
--- controller/CategoryController.php
+++ controller/CategoryController.php
@@ -1,17 +1,17 @@
 <?php
 class CategoryController extends BaseController {
-	
-	public function indexAction() {
-		$cid = $this->param ( 'cid' );
-		$category = CategoryData::getById ( $cid );
-		$group = GroupData::getById ( $category ['group_id'] );
-		$store = StoreData::getById ( $category ['store_id'] );
-		$goods = GoodsData::getsByCid ( $cid );
-		$this->assign ( 'category', $category );
-		$this->assign ( 'group', $group );
-		$this->assign ( 'store', $store );
-		$this->assign ( 'goods', $goods );
-		/* if (isset ( $_SESSION ['cart'] [$cid] )) {
+    
+    public function indexAction() {
+        $cid = $this->param ( 'cid' );
+        $category = CategoryData::getById ( $cid );
+        $group = GroupData::getById ( $category ['group_id'] );
+        $store = StoreData::getById ( $category ['store_id'] );
+        $goods = GoodsData::getsByCid ( $cid );
+        $this->assign ( 'category', $category );
+        $this->assign ( 'group', $group );
+        $this->assign ( 'store', $store );
+        $this->assign ( 'goods', $goods );
+        /* if (isset ( $_SESSION ['cart'] [$cid] )) {
 			$curCart = $_SESSION ['cart'] [$cid];
 			$totalPrice = 0;
 			foreach ( $curCart as &$v ) {
@@ -21,9 +21,10 @@ class CategoryController extends BaseController {
 			$this->assign ( 'curCart', $curCart );
 			$this->assign ( 'totalPrice', $totalPrice );
 		} */
-		$cart = ComTool::getCart ( $cid );
-		$this->assign ( 'products', $cart ['products'] );
-		$this->assign ( 'totalPrice', $cart ['totalPrice'] );
-		$this->display ();
-	}
+        $cart = array ();
+        $cart = $this->getCart ( $cid );
+        $this->assign ( 'products', $cart ['products'] );
+        $this->assign ( 'totalPrice', $cart ['totalPrice'] );
+        $this->display ();
+    }
 }
