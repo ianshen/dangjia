2420dea0cc583fc77da9591a3d9c6a4863d17292
ianshen <ian.shen.sense@gmail.com>
2014-04-25 18:41:24 +0800

    82

---
+++ controller/RumourController.php
@@ -0,0 +1,15 @@
+<?php
+class RumourController extends BaseController {
+    
+    public function indexAction() {
+        $this->display ();
+    }
+    
+    public function addAction() {
+    
+    }
+    
+    public function removeAction() {
+    
+    }
+}
