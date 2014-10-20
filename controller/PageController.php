d0400af99848d4c89c73f0870f255464647a97bb
ianshen <ian.shen.sense@gmail.com>
2014-10-20 23:19:13 +0800

    125

--- controller/PageController.php
+++ controller/PageController.php
@@ -1,18 +1,18 @@
 <?php
 class PageController extends BaseController {
-    
-    public function indexAction() {
-        $sid = $this->param ( 'sid' );
-    }
-    
-    /**
-     * 店铺page
-     */
-    public function storeAction() {
-        $this->display ();
-    }
-    
-    public function showAction() {
-    
-    }
+	
+	public function indexAction() {
+		$sid = $this->param ( 'sid' );
+	}
+	
+	/**
+	 * 店铺page
+	 */
+	public function storeAction() {
+		$this->display ();
+	}
+	
+	public function showAction() {
+		$this->display ();
+	}
 }
